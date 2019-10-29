2a3e7444347ef88c8d1843a663765184766a22e9
SHARMILAG97 <srdssharmig97@gmail.com>
2019-10-29 13:03:22 +0530

    Cors Config Changes

--- src/main/java/com/revature/watercanappstockms/config/CorsConfig.java
+++ src/main/java/com/revature/watercanappstockms/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
    public void addCorsMappings(CorsRegistry registry) {
        registry.addMapping("/**")
-               .allowedOrigins("http://localhost:8081")
+               .allowedOrigins("http://localhost:4200")
                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD")
                .allowCredentials(true);
     } }
